da0c6929c0ec6cedd6ae298e7ba99cf33e106924
nilakande <shine.neelu@gmail.com>
2024-05-06 08:57:10 +0530

    changes

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+//for music video
+(function($) {
+  $(function() {
+    $('nav ul li > a:not(:only-child)').click(function(e) {
+      $(this).siblings('.nav-dropdown').toggle();
+      $('.nav-dropdown').not($(this).siblings()).hide();
+      e.stopPropagation();
+    });
+    $('html').click(function() {
+      $('.nav-dropdown').hide();
+    });
+    $('#nav-toggle').on('click', function() {
+      this.classList.toggle('active');
+    });
+    $('#nav-toggle').click(function() {
+  $('nav ul').toggle();
+});
+  });
+})(jQuery);
+//for music play
+const player = new Plyr('#video-player');
+player.on('play', event => {
+    $('#video-player').removeClass('active');
+});
+player.on('ended', event => {
+    $('#video-player').addClass('active');
+});
+//end
 const menu = document.querySelector(".menu");
 const menuInner = menu.querySelector(".menu__inner");
 const menuArrow = menu.querySelector(".menu__arrow");
